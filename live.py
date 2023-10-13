8b33db749dc4805121d8bf1296aafa81458acf1d
Michael Neumayr <michael.jun.neumayr@gmail.com>
2023-10-14 00:51:49 +0200

    face detector & emotion detection network with fps

--- live.py
+++ live.py
@@ -1,68 +1,88 @@
+import time
 import cv2
 import numpy as np
-from batch_face import RetinaFace
+from face_detection.detector import RetinaFace
 
 import torch
 import torch.nn as nn
-import torchvision.models as models
-from torchvision.models.resnet import ResNet18_Weights
 import torch.nn.functional as F
+
+import torchvision.models as models
+from torchvision.models.mobilenetv3 import MobileNet_V3_Large_Weights
 from torchvision.transforms import transforms
 
 
 def live_emotion_detection(MODEL_PATH, device, emotion_labels):
-    detector = RetinaFace(gpu_id=0)
-
-    model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
-    num_ftrs = model.fc.in_features
-    model.fc = nn.Sequential(
-        nn.Dropout(0.5),
-        nn.Linear(num_ftrs, 7)
-    )
+    model = models.mobilenet_v3_large(weights=MobileNet_V3_Large_Weights.DEFAULT)
+    model.classifier[3] = nn.Linear(1280, 7)
     model.to(device)
     model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.eval()
+
+    NUM_FRAMES = 5
+    frame_count = 0
+    fps = 0
+    start_time = time.time()
+
+    detector = RetinaFace(device=device)
 
     # Start video capture
     cap = cv2.VideoCapture(0)
 
     while True:
-        # Capture frame-by-frame
         ret, frame = cap.read()
-
-        # Resize frame
         frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
 
-        # Detect faces in the frame
-        faces, landmarks = detector.detect(frame, threshold=0.5, scale=1.0)
+        faces = detector.detect(frame)
 
         # Loop through detected faces
-        for i in range(faces.shape[0]):
-            box = faces[i].astype(np.int)
+        for i in faces:
+            face, landmarks, certainty = i
+            box = face.astype(np.int32)
             (x, y, x1, y1) = box
+            x = max(0, x)
+            y = max(0, y)
+            x1 = min(frame.shape[1], x1)
+            y1 = min(frame.shape[0], y1)
+
             roi_face = frame[y:y1, x:x1]
 
             # Preprocess face for emotion recognition
             transform = transforms.Compose([
+                transforms.ToPILImage(),
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
             input_tensor = transform(roi_face)
             input_batch = input_tensor.unsqueeze(0)
+            input_batch = input_batch.to(device)
 
             # Predict emotion
-            with torch.no_grad(), torch.cuda.amp.autocast():
+            with torch.no_grad():
                 output = model(input_batch)
             output = F.softmax(output, dim=1)
-            emotion = emotion_labels[torch.argmax(output).item()]
+            confidence = torch.max(output).item()
+            emotion_idx = torch.argmax(output).item()
+            emotion = emotion_labels[emotion_idx]
 
             # Draw box and label on face
             cv2.rectangle(frame, (x, y), (x1, y1), (0, 0, 255), 2)
-            cv2.putText(frame, emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            cv2.putText(frame, f"{emotion} ({confidence:.2f})", (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1, cv2.LINE_AA)
+
+        # Update frame count and check if benchmarking is complete
+        frame_count += 1
+        if frame_count == NUM_FRAMES:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            fps = NUM_FRAMES / elapsed_time
+            frame_count = 0
+            start_time = time.time()
+        cv2.putText(frame, f"FPS: {fps:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -82,6 +102,6 @@ if __name__ == "__main__":
         6: "Neutral"
     }
     MODEL_PATH = './models/'
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
     live_emotion_detection(MODEL_PATH, device, class_dict)
